290d45baf21b7a76140516f1f34873e11631d83b
Durfan <cecilio@aluno.ufsj.edu.br>
2019-10-25 10:52:32 -0300

    working vs

--- autoimports/automoney/admin.py
+++ autoimports/automoney/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 
 from .models import Equipamento, Compra, Venda
 
-admin.site.register(Equipamento)
-admin.site.register(Compra)
-admin.site.register(Venda)
+class EquipamentoAdmin(admin.ModelAdmin):
+	model = Equipamento
+	list_display = ('nome', 'cor', 'quantidade', 'logistica')
+
+admin.site.register(Equipamento, EquipamentoAdmin)
+
+class CompraAdmin(admin.ModelAdmin):
+	model = Compra
+	list_display = ['data', 'quantidade', 'get_name', 'get_vcompra']
+
+admin.site.register(Compra, CompraAdmin)
+
+class VendaAdmin(admin.ModelAdmin):
+	model = Venda
+	list_display = ['data', 'quantidade', 'get_name', 'get_vvenda']
+
+admin.site.register(Venda, VendaAdmin)
--- autoimports/automoney/models.py
+++ autoimports/automoney/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
+
 class Equipamento(models.Model):
 	nome = models.CharField(max_length=254)
 	cor = models.CharField(max_length=128)
-	descricao = models.TextField()
+	descricao = models.TextField("Descrição")
 	quantidade = models.IntegerField()
-	valorcompra = models.DecimalField(max_digits=19, decimal_places=2)
-	valovenda = models.DecimalField(max_digits=19, decimal_places=2)
-	logistica = models.BooleanField()
+	valorcompra = models.DecimalField("Valor de Compra", max_digits=19, decimal_places=2)
+	valorvenda = models.DecimalField("Valor de Venda", max_digits=19, decimal_places=2)
+	logistica = models.BooleanField("Requer Compra")
 
 	class Meta:
 		db_table = 'equipamento'
@@ -18,17 +19,41 @@ class Equipamento(models.Model):
 
 class Compra(models.Model):
 	data = models.DateTimeField()
-	equip = models.ForeignKey('Equipamento', on_delete=models.DO_NOTHING)
+	quantidade = models.IntegerField()
+	equip = models.ForeignKey('Equipamento', models.DO_NOTHING, verbose_name="Equipamento")
 
 	class Meta:
 		db_table = 'compra'
 
+	def __str__(self):
+		return self.equip.nome
+
+	def get_name(self):
+		return self.equip.nome
+
+	def get_vcompra(self):
+		return self.equip.valorcompra
+
+	get_name.short_description = 'Equipamento'
+	get_vcompra.short_description = 'Valor da Compra'
+
 
 class Venda(models.Model):
 	data = models.DateTimeField()
-	equip = models.IntegerField()
+	quantidade = models.IntegerField()
+	equip = models.ForeignKey('Equipamento', models.DO_NOTHING, verbose_name="Equipamento")
 
 	class Meta:
 		db_table = 'venda'
 
+	def __str__(self):
+		return self.equip.nome
 
+	def get_name(self):
+		return self.equip.nome
+
+	def get_vvenda(self):
+		return self.equip.valorvenda
+
+	get_name.short_description = 'Equipamento'
+	get_vvenda.short_description = 'Valor da Venda'
